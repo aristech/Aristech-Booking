be1ee222c9a040199babed952197b86b58e726d2
aristech <aristech.gr@gmail.com>
2018-04-22 01:07:49 +0300

    Update and rename unistall.php to uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -13,6 +13,7 @@ global $wpdb;
 $array = array( 'aristech_booking_url','aristech_title','aristech_text','aristech_btn','aristech_tel','aristech_radio','aristech_color_tt','aristech_color_tb','aristech_color_st','aristech_color_ft','aristech_color_fb','aristech_color_bt','aristech_color_bb','aristech_image',);
 
 foreach ($array as $item) {
+    $item = esc_sql( $item );
     $wpdb->query( "DELETE FROM wp_options WHERE option_name = '$item'" );
 }
 
